c64d78acee5c2bd3d7a6dca2d5f3a6ae3d573d29
Mikko85git <55048370+Mikko85git@users.noreply.github.com>
2019-10-08 18:21:28 +0300

    html valikonrunko valmis

---
+++ Valikko.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<div id="Etusivu">
+Tämä on etusivu
+</div>
+<div id="Kirjaudu sisään">
+  Tähän tulee kirjautumis sivu
+</div>
+<div id="Luo käyttäjätunnus">
+  Tähän tulee käyttätunnuksen luonti
+</div>
+<div id="Selaa tuotteita">
+//  Tuotteiden selaaminen (toimii ilman kirjautumista)
+</div>
+<div id="Ostoskori">
+// näkyy kirjautuneelle asiakkaalle
+</div>
+<div id="Tietokannat">
+//  Tämä näkyy ainostaan masterkäyttäjälle
+</div>
+<div id="Kirjaudu ulos">
+// Näkyy asinoastaan kirjautuneelle
+</div>
+  </body>
+</html>
